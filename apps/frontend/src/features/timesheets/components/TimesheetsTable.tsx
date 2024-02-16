77f68ec059d42b830e73a2d46570ac7de906faf2
Luis Alejandro Calvo Alvarez <offztinger@gmail.com>
2024-02-16 02:05:07 -0500

    Implementando funciones para notas y estado en timesheet

--- apps/frontend/src/features/timesheets/components/TimesheetsTable.tsx
+++ apps/frontend/src/features/timesheets/components/TimesheetsTable.tsx
@@ -2,14 +2,26 @@ import MUIDataTable, {
   MUIDataTableColumnDef,
   MUIDataTableMeta,
   MUIDataTableOptions,
-} from "mui-datatables";
-import { Typography, CircularProgress, Stack, Select, MenuItem } from "@mui/material";
-import { MUIDataTableDefaultOptions } from "@/constants/muidatatable.constants";
-import { Add, Details } from "@mui/icons-material";
-import Button from "@mui/material/Button";
-import useDeleteTimesheets from "../hooks/useDeleteTimesheets";
-import { ITimesheets, TimesheetStatus } from "../models/Timesheets.type";
-import { useTimesheetsContext } from "../context/Timesheets.context";
+} from 'mui-datatables';
+import {
+  Typography,
+  CircularProgress,
+  Stack,
+  Select,
+  MenuItem,
+} from '@mui/material';
+import { MUIDataTableDefaultOptions } from '@/constants/muidatatable.constants';
+import { Add, Details } from '@mui/icons-material';
+import Button from '@mui/material/Button';
+import useDeleteTimesheets from '../hooks/useDeleteTimesheets';
+import { ITimesheets, TimesheetStatus } from '../models/Timesheets.type';
+import { useTimesheetsContext } from '../context/Timesheets.context';
+import useUpdateStatusTimesheets from '../hooks/useUpdateStatusTimesheets';
+import { useSelector } from 'react-redux';
+import { AppStore } from '@/redux/store';
+import toastsManager from '@/utilities/toasts.manager';
+import TimesheetModal from './TimesheetModal';
+import { useEffect, useState } from 'react';
 
 interface Props {
   Timesheets: ITimesheets[];
@@ -17,7 +29,11 @@ interface Props {
   updateTable: () => void;
 }
 
-export default function TimesheetsTable({ Timesheets, loading, updateTable }: Props) {
+export default function TimesheetsTable({
+  Timesheets,
+  loading,
+  updateTable,
+}: Props) {
   const {
     setGroupToEdit,
     openEditGroupDialog,
@@ -25,127 +41,201 @@ export default function TimesheetsTable({ Timesheets, loading, updateTable }: Pr
     setIsEdit,
   } = useTimesheetsContext();
 
+  const getRole = useSelector((state: AppStore) => state.authState.user.role);
+
   const { deleteTimesheet } = useDeleteTimesheets();
+  const { updateTimesheets } = useUpdateStatusTimesheets();
+
   const handleEditGroup = (Timesheets: ITimesheets) => {
     setGroupToEdit(Timesheets);
   };
   const options: MUIDataTableOptions = {
     ...MUIDataTableDefaultOptions,
-    searchPlaceholder:
-      "Buscar",
+    searchPlaceholder: 'Buscar',
   };
   const handleEditClick = (dataTable: MUIDataTableMeta<unknown>) => {
     setIsEdit(true);
-    setTitleGroupDialog("Edit Employee");
+    setTitleGroupDialog('Edit Employee');
     handleEditGroup(Timesheets[dataTable.rowIndex]);
     openEditGroupDialog();
   };
 
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const [modalType, setModalType] = useState('');
+  const [idToEdit, setIdToEdit] = useState('');
+  const [notesToEdit, setNotesToEdit] = useState('');
+
   const columns: MUIDataTableColumnDef[] = [
-    { name: "id", options: { display: false } },
+    { name: 'id', options: { display: false } },
     {
-      name: "payPeriodStart",
-      label: "payPeriodStart",
+      name: 'payPeriodStart',
+      label: 'payPeriodStart',
       options: {
-        customBodyRender: (payPeriodStart: any, dataTable: { rowData: any[]; }) => {
+        customBodyRender: (
+          payPeriodStart: any,
+          dataTable: { rowData: any[] },
+        ) => {
           return (
             <Typography
               sx={{
-                cursor: "pointer",
-                textDecoration: "underline",
-                color: "blue",
+                cursor: 'pointer',
+                textDecoration: 'underline',
+                color: 'blue',
               }}
               onClick={() => handleEditClick(dataTable)}
-            >{payPeriodStart}</Typography>
+            >
+              {payPeriodStart}
+            </Typography>
           );
         },
       },
     },
     {
-      name: "payPeriodEnd",
-      label: "payPeriodEnd",
+      name: 'payPeriodEnd',
+      label: 'payPeriodEnd',
       options: {
-        customBodyRender: (payPeriodEnd: any, dataTable: { rowData: any[]; }) => {
+        customBodyRender: (
+          payPeriodEnd: any,
+          dataTable: { rowData: any[] },
+        ) => {
           return (
             <Typography
               sx={{
-                cursor: "pointer",
-                textDecoration: "underline",
-                color: "blue",
+                cursor: 'pointer',
+                textDecoration: 'underline',
+                color: 'blue',
               }}
               onClick={() => handleEditClick(dataTable)}
-            >{payPeriodEnd}</Typography>
+            >
+              {payPeriodEnd}
+            </Typography>
           );
         },
       },
     },
     {
-      name: "grossPayroll",
-      label: "grossPayroll",
+      name: 'grossPayroll',
+      label: 'grossPayroll',
     },
     {
-      name: "notes",
-      label: "notes",
+      name: 'notes',
+      label: 'notes',
     },
     {
-      name: "checkDate",
-      label: "checkDate",
+      name: 'checkDate',
+      label: 'checkDate',
     },
     {
-      name: "status",
-      label: "status",
+      name: 'status',
+      label: 'status',
       options: {
         customBodyRender: (status: string) => {
-          return(
-          <Select
-            value={status}
-            onChange={()=> {}}
-          >
-            <MenuItem value={TimesheetStatus?.APPROVED}>{TimesheetStatus?.APPROVED}</MenuItem>
-            <MenuItem value={TimesheetStatus?.PENDING}>{TimesheetStatus?.PENDING}</MenuItem>
-            <MenuItem value={TimesheetStatus?.REJECTED}>{TimesheetStatus?.REJECTED}</MenuItem>
-          </Select> 
-          )
-        }
-      }
+          return (
+            <Select
+              value={status}
+              onChange={(e) => {
+                if (getRole == 'Customer') {
+                  toastsManager.showToast(
+                    'warning',
+                    "You don't have permission to change the status",
+                  );
+                } else if (getRole == 'Admin') {
+                  updateTimesheets({
+                    id: Timesheets[0].id,
+                    status: e.target.value as string,
+                  }).finally(() => {
+                    toastsManager.showToast(
+                      'success',
+                      'The status has been updated successfully',
+                    );
+                    updateTable();
+                  });
+                }
+              }}
+            >
+              <MenuItem value={TimesheetStatus?.APPROVED}>
+                {'Approved'}
+              </MenuItem>
+              <MenuItem value={TimesheetStatus?.PENDING}>{'Pending'}</MenuItem>
+              <MenuItem value={TimesheetStatus?.REJECTED}>
+                {'Rejected'}
+              </MenuItem>
+            </Select>
+          );
+        },
+      },
     },
     {
-      name: "id",
-      label: "Options",
+      name: 'id',
+      label: 'Options',
       options: {
-        customBodyRender: (id: string) => {
-          return(
-            <Stack flexDirection="row" justifyContent="space-between" alignItems="center">
-              <Button variant="outlined" startIcon={<Details />} onClick={async () => {
-              }}>DETAIL</Button>
-              <Button variant="outlined" startIcon={<Add />} onClick={async () => {
-              }}>ADD NOTE</Button>
-           </Stack>
-          )
+        customBodyRender: (id: string, meta: MUIDataTableMeta<unknown>) => {
+          return (
+            <Stack
+              flexDirection="row"
+              justifyContent="space-between"
+              alignItems="center"
+            >
+              <Button
+                variant="outlined"
+                startIcon={<Details />}
+                onClick={async () => {
+                  setModalType('details');
+                  setIdToEdit(id);
+                  handleOpen();
+                }}
+              >
+                DETAIL
+              </Button>
+              <Button
+                variant="outlined"
+                startIcon={<Add />}
+                onClick={async () => {
+                  setModalType('notes');
+                  setIdToEdit(id);
+                  handleOpen();
+                  setNotesToEdit(meta.rowData[4] as string);
+                }}
+              >
+                ADD NOTE
+              </Button>
+            </Stack>
+          );
         },
         setCellProps: () => ({
           width: '300px',
-        })
-
+        }),
       },
     },
   ];
 
   return (
-    <MUIDataTable
-      title={
-        loading ? (
-          <Typography>
-            Cargando...
-            <CircularProgress size={20} />
-          </Typography>
-        ) : (
-          "List Timesheets"
-        )
-      }
-      data={Timesheets}
-      columns={columns}
-      options={options}
-    ></MUIDataTable>
+    <div>
+      <TimesheetModal
+        open={open}
+        handleClose={handleClose}
+        modalType={modalType}
+        idToEdit={idToEdit}
+        notesToEdit={notesToEdit}
+        updateTable={updateTable}
+      />
+      <MUIDataTable
+        title={
+          loading ? (
+            <Typography>
+              Cargando...
+              <CircularProgress size={20} />
+            </Typography>
+          ) : (
+            'List Timesheets'
+          )
+        }
+        data={Timesheets}
+        columns={columns}
+        options={options}
+      ></MUIDataTable>
+    </div>
   );
 }
--- apps/frontend/src/features/timesheets/containers/TimesheetsContainer.tsx
+++ apps/frontend/src/features/timesheets/containers/TimesheetsContainer.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from "react";
-import useGetTimesheets from "../hooks/useGetTimesheets";
-import { ITimesheets } from "../models/Timesheets.type";
-import { TimesheetsProvider } from "../context/Timesheets.context";
-import TimesheetsTable from "../components/TimesheetsTable";
-import TimesheetsEditDialog from "../components/TimesheetsEditDialog";
-import TimesheetsSettings from "../components/TimesheetsSettings";
+import React, { useEffect, useState } from 'react';
+import useGetTimesheets from '../hooks/useGetTimesheets';
+import { ITimesheets } from '../models/Timesheets.type';
+import { TimesheetsProvider } from '../context/Timesheets.context';
+import TimesheetsTable from '../components/TimesheetsTable';
+import TimesheetsEditDialog from '../components/TimesheetsEditDialog';
+import TimesheetsSettings from '../components/TimesheetsSettings';
 
 const TimesheetsContainer = () => {
   const { getTimesheets } = useGetTimesheets();
@@ -12,15 +12,20 @@ const TimesheetsContainer = () => {
   const getTimesheetsFromApi = async () => {
     const Timesheets = await getTimesheets();
     setTimesheetsData(Timesheets.data.timesheets);
-  }
+  };
 
   useEffect(() => {
     getTimesheetsFromApi();
-  }, [])
+  }, []);
+
   return (
     <TimesheetsProvider>
       <TimesheetsSettings />
-      <TimesheetsTable Timesheets={TimesheetsData || []} loading={false} updateTable={getTimesheetsFromApi} />
+      <TimesheetsTable
+        Timesheets={TimesheetsData || []}
+        loading={false}
+        updateTable={getTimesheetsFromApi}
+      />
       <TimesheetsEditDialog getTimesheetsFromApi={getTimesheetsFromApi} />
     </TimesheetsProvider>
   );
